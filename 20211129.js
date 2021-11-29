2d5010d3facada11e75e4be74ddcf3e887ced780
Gintare Raudziute <gintare.raudziute@gmail.com>
2021-11-29 11:50:12 +0200

    added number generator

--- 20211129.js
+++ 20211129.js
@@ -1,1 +1,12 @@
 const h = document.querySelector("h1");
+const btn = document.querySelector(".btn");
+
+function randomNum (){
+    return Math.round(Math.random()*100)
+}
+
+h.innerHTML = `${randomNum()}`;
+
+btn.onclick = () => {
+    h.innerHTML = `${randomNum()}`;
+}
